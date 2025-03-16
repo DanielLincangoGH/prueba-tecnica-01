19fbb403f63af98670680e14bb3fbac67b27cce8
Daniel Lincango <flincango@innovadevs.com>
2025-03-16 12:17:04 -0500

    Brand initializer test

--- src/test/java/com/hiberus/hiring/infrastructure/config/BrandInitializerConfigTest.java
+++ src/test/java/com/hiberus/hiring/infrastructure/config/BrandInitializerConfigTest.java
@@ -2,9 +2,9 @@ package com.hiberus.hiring.infrastructure.config;
 
 import static org.mockito.Mockito.verify;
 
-import com.hiberus.hiring.application.service.BrandServiceImpl;
+import com.hiberus.hiring.application.command.BrandCommandService;
+import com.hiberus.hiring.domain.enums.BrandEnum;
 import com.hiberus.hiring.domain.model.Brand;
-import com.hiberus.hiring.domain.ports.out.BrandRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,25 +14,28 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-class BrandServiceImplTest {
+class BrandInitializerConfigTest {
 
   @Mock
-  private BrandRepository brandRepository;
+  private BrandCommandService brandService;
 
   @InjectMocks
-  private BrandServiceImpl brandService;
+  private BrandInitializerConfig brandInitializerConfig;
 
   private Brand brand;
 
   @BeforeEach
   void setUp() {
-    brand = new Brand();
+    brand = Brand.builder()
+        .brandId(BrandEnum.HIBERUS.getId())
+        .name(BrandEnum.HIBERUS.getName())
+        .build();
   }
 
   @Test
-  @DisplayName("Success: Given a brand, when create, then persist the brand")
-  void givenABrandWhenCreateThenPersistTheBrand() {
-    brandService.create(brand);
-    verify(brandRepository).create(brand);
+  @DisplayName("Success: Given a brand, when init is called, then create the brand")
+  void givenABrandWhenInitIsCalledThenCreateTheBrand() {
+    brandInitializerConfig.init();
+    verify(brandService).create(brand);
   }
 }
